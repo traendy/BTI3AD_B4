b3ac1adb58ebd7bf1847a23b3d2297255f8ac50f
Sönke Peters <peters.soenke@googlemail.com>
2017-04-17 09:17:44 +0200

    Added Counter

--- AD_Blatt4/src/PascalBinomial.java
+++ AD_Blatt4/src/PascalBinomial.java
@@ -2,16 +2,62 @@ import java.math.BigInteger;
 
 public class PascalBinomial {
   private int[] result;
+  /** The counter. */
+  private Counter  counter = null;
   
   
   
+  
+  public PascalBinomial() {
+    super();
+  }
+
+  
+  public PascalBinomial(Counter c) {
+    this();
+    this.counter = c;
+  }
+
+
   public int[] dreieck(BigInteger N){
     result = new int[N.intValue()+1];
    
-    Binomialkoeffezient binom = new Binomialkoeffezient();
     for(int i = 0; i<=N.intValue(); i++){
-      result[i]=binom.binomialCoefficient(N, BigInteger.valueOf(i)).intValue();
+      result[i]=binomialCoefficient(N, BigInteger.valueOf(i)).intValue();
+      count();
     }
     return result;
   }
+  
+  private void count() {
+    if(counter != null){
+        counter.counterUp(1);
+    }
+  }
+    
+    
+    
+    public  BigInteger binomialCoefficient(BigInteger n,BigInteger k){
+      
+      BigInteger n_minus_k=n.subtract(k);
+      if(n_minus_k.compareTo(k)<0){
+          BigInteger temp=k;
+          k=n_minus_k;
+          n_minus_k=temp;
+      }
+      
+      BigInteger numerator=BigInteger.ONE;
+      BigInteger denominator=BigInteger.ONE;
+      
+      for(BigInteger j=BigInteger.ONE; j.compareTo(k)<=0; j=j.add(BigInteger.ONE)){
+          numerator=numerator.multiply(j.add(n_minus_k));
+          denominator=denominator.multiply(j);
+          BigInteger gcd=numerator.gcd(denominator);
+          numerator=numerator.divide(gcd);
+          denominator=denominator.divide(gcd);
+      }
+      
+      return numerator;
+  }
 }
+
